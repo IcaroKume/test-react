5f0f489c575184d3962de25b5d108d1647a7c7cc
IcaroKume <icarokume@MacBook-Pro-de-Icaro.local>
2016-01-25 23:47:42 -0200

    material ui

--- app/main.js
+++ app/main.js
@@ -5,24 +5,82 @@ import { Router, Route, IndexRoute, Link} from 'react-router'
 import { browserHistory } from 'react-router'
 import Home from './routes/Home.jsx';
 import News from './routes/News.jsx';
+import mui from 'material-ui';
+import List from 'material-ui/lib/lists/list';
+import ListItem from 'material-ui/lib/lists/list-item';
+import ThemeManager from 'material-ui/lib/styles/theme-manager';
+import LightRawTheme from 'material-ui/lib/styles/raw-themes/light-raw-theme';
+import Colors from 'material-ui/lib/styles/colors';
+import {SelectableContainerEnhance} from 'material-ui/lib/hoc/selectable-enhance';
+
+const SelectableList = SelectableContainerEnhance(List);
+
+const AppBar = mui.AppBar
+  , LeftNav = mui.LeftNav
+  , MenuItem = mui.MenuItem;
+
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
 
 var intlData = {
     "greeting": "Hello my name is {name}.\n"
 };
 
 const App = React.createClass({
-  render() {
-    return (
-      <div>
-        <h1>Test React</h1>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/news">News</Link></li>
-        </ul>
-        {this.props.children}
-      </div>
-    )
-  }
+
+    childContextTypes: {
+        muiTheme: React.PropTypes.object,
+    },
+
+    getInitialState() {
+        return {
+          muiTheme: ThemeManager.getMuiTheme(LightRawTheme),
+          open: false,
+        };
+    },
+
+    getChildContext() {
+        return {
+          muiTheme: this.state.muiTheme,
+        };
+    },
+
+    componentWillMount() {
+        let newMuiTheme = ThemeManager.modifyRawThemePalette(this.state.muiTheme, {
+          accent1Color: Colors.deepOrange500,
+        });
+
+        this.setState({muiTheme: newMuiTheme});
+    },
+
+    handleToggle() {this.setState({open: !this.state.open})},
+
+    handleRequestChangeList() {this.setState({open: false})},
+
+    render() {
+
+        return (
+          <div>
+              <LeftNav docked={false} open={this.state.open} onRequestChange={open => this.setState({open})}>
+                  <SelectableList valueLink={{value: "", requestChange: this.handleRequestChangeList}}>
+                      <ListItem>
+                          <Link to="/">Home</Link>
+                      </ListItem>
+                      <ListItem>
+                          <Link to="/news">News</Link>
+                      </ListItem>
+                  </SelectableList>
+              </LeftNav>
+
+              <header>
+                <AppBar title='ZoomFleet' onLeftIconButtonTouchTap={this.handleToggle}
+                  isInitiallyOpen={true}
+                  />
+              </header>
+            {this.props.children}
+          </div>
+        )
+    }
 })
 
 render((
